1342df95882a8a69e9c7449ddcbb71179e8f1a4d
Hammond <hieutruonghoang01@gmail.com>
2023-12-28 15:30:07 +0700

    created nav bar

--- client/src/Components/NavBar/NavBar.js
+++ client/src/Components/NavBar/NavBar.js
@@ -1,23 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../Data/logo.png';
 import cartIcon from '../Data/cart_icon.png';
 import './NavBar.css';
 const NavBar = () => {
+    const [menu, setMenu] = useState("shop");
     return (
-        <div className='flex justify-around p-4'>
+        <div className='navbar'>
             <div className='nav-logo'>
                 <img src={logo} alt='Logo' />
                 <p>Ecommerce</p>
             </div>
-            <ul className='nav-menu text-red-500'>
-                <li>SHOP</li>
-                <li>MEN</li>
-                <li>WOMEN</li>
-                <li>KIDS</li>
+            <ul className='nav-menu'>
+                <li onClick={() => { setMenu("shop") }}>Shop {menu === 'shop' ? <hr /> : <></>}</li>
+                <li onClick={() => { setMenu("mens") }}>Men {menu === 'mens' ? <hr /> : <></>}</li>
+                <li onClick={() => { setMenu("women") }}>Women {menu === 'women' ? <hr /> : <></>}</li>
+                <li onClick={() => { setMenu("kids") }}>Kids {menu === 'kids' ? <hr /> : <></>}</li>
             </ul>
             <div className="nav-login-cart">
                 <button>Login</button>
                 <img src={cartIcon} alt='Logo' />
+                <div className="nav-cart-count">0</div>
             </div>
         </div>
     )
