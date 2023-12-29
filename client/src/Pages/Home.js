3cc50732e5de1284243d4346ecf9250417686e82
Hammond <hieutruonghoang01@gmail.com>
2023-12-29 11:41:21 +0700

    Created components Offers and Popular

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import ContentTop from '../Components/Content/ContentTop'
+import Popular from '../Components/Popular/Popular'
+import Offer from '../Components/Offer/Offer'
 
 const Home = () => {
     return (
         <div>
             <ContentTop />
+            <Popular />
+            <Offer />
         </div>
     )
 }
