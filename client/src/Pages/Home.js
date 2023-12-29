b3187c909a393f34d65a31b2bd6f88be0b903fc0
Hammond <hieutruonghoang01@gmail.com>
2023-12-29 12:27:43 +0700

    created components News letter

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -3,6 +3,7 @@ import ContentTop from '../Components/Content/ContentTop'
 import Popular from '../Components/Popular/Popular'
 import Offer from '../Components/Offer/Offer'
 import NewCollections from '../Components/NewCollections/NewCollections'
+import NewsLetter from '../Components/NewsLetter/NewsLetter'
 
 const Home = () => {
     return (
@@ -11,6 +12,7 @@ const Home = () => {
             <Popular />
             <Offer />
             <NewCollections />
+            <NewsLetter />
         </div>
     )
 }
