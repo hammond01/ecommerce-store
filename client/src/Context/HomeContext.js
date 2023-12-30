3823dd5538373d05fc7246bcbe13c4bb72b69bfc
Hammond <hieutruonghoang01@gmail.com>
2023-12-30 21:58:17 +0700

    config HomeContext

---
+++ client/src/Context/HomeContext.js
@@ -0,0 +1,13 @@
+import React, { createContext } from "react";
+import all_product from '../Components/Data/all_product'
+export const HomeContext = createContext(null);
+
+const HomeContextProvider = (props) => {
+    const contextValue = { all_product };
+    return (
+        <HomeContext.Provider value={contextValue}>
+            {props.children}
+        </HomeContext.Provider>
+    )
+}
+export default HomeContextProvider;
