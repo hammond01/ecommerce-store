faec205f71406f68921831e34ba4af0ee386cced
Hammond <hieutruonghoang01@gmail.com>
2023-12-31 22:03:09 +0700

    Signup screen  demo

--- client/src/Pages/Auth/Login.js
+++ client/src/Pages/Auth/Login.js
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import './Auth.css'
 const Login = () => {
     return (
-        <div>
-            Login
+        <div className='login'>
+            <div className="login-container">
+                <h1>Sign up</h1>
+                <div className="signup-fields">
+                    <input type="text" placeholder='Enter your Name' />
+                    <input type="email" placeholder='Enter your Email' />
+                    <input type="password" placeholder='Enter your Password' />
+                </div>
+                <button>Continue</button>
+                <p className='login-login'>Already have an account?<span>Login here</span> </p>
+                <div className="login-agree">
+                    <input type="checkbox" name="" id="" />
+                    <p>By continuing, I agree to the terms of use & privacy policy.</p>
+                </div>
+            </div>
         </div>
     )
 }
