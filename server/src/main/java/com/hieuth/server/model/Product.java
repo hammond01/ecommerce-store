9ed3b68bb644675d3f5e76f56439267b00545aa6
Hammond <hieutruonghoang01@gmail.com>
2024-01-04 22:49:23 +0700

    Created model Product

---
+++ server/src/main/java/com/hieuth/server/model/Product.java
@@ -0,0 +1,13 @@
+package com.hieuth.server.model;
+/**
+ * @author Hoàng Hiếu
+ * */
+public class Product {
+    private Long id;
+    private String name;
+    private String image;
+    private int new_price;
+    private int old_price;
+    private boolean available;
+
+}
