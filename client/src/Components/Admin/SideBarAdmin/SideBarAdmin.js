d8c3cae1cec3309511fa0b157e08eb3c7e2158f8
Hammond <hieutruonghoang01@gmail.com>
2024-01-09 23:56:42 +0700

    Update sibar admin

--- client/src/Components/Admin/SideBarAdmin/SideBarAdmin.js
+++ client/src/Components/Admin/SideBarAdmin/SideBarAdmin.js
@@ -1,19 +1,19 @@
 import React from 'react';
-// import './SideBarAdmin.css';
+import './SideBarAdmin.css';
 import { Link } from 'react-router-dom';
 import addProduct_icon from '../../../Asset/Admin/Product_Cart.svg';
 import listProduct_icon from '../../../Asset/Admin/Product_list_icon.svg';
 
 const SideBarAdmin = () => {
 	return (
-		<div className="sidebar-admin flex h-[100vh] w-full max-w-[250px] flex-col gap-5 bg-white pt-[30px]">
-			<Link to={'/addProduct'} style={{ textDecoration: 'none' }}>
+		<div className="sidebar-admin">
+			<Link to={'/admin/addProduct'} style={{ textDecoration: 'none' }}>
 				<div className="sidebar-item">
 					<img src={addProduct_icon} alt="Product Icon" />
 					<p>Add product</p>
 				</div>
 			</Link>
-			<Link to={'/listProduct'} style={{ textDecoration: 'none' }}>
+			<Link to={'/admin/listProduct'} style={{ textDecoration: 'none' }}>
 				<div className="sidebar-item">
 					<img src={listProduct_icon} alt="Product Icon" />
 					<p>List product</p>
